9f4f8e67e0f06e95f1c811b9e64584c2448cde2f
Глеб <@HOME-PC>
2024-01-23 16:57:43 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1 
+            //for(int a=1;a<=10;a++)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //Console.ReadKey();
+            //int a = 1;
+            //while(a<=10)
+            //{
+            //    Console.WriteLine(a);
+            //    a++;
+            //}
+            //Console.ReadKey();
+            //int a = 1;
+            //    do
+            //{
+            //    Console.WriteLine(a);
+            //    a++;
+            //} while (a <= 10);
+            //Console.ReadKey();
+            //Задание 2
+
+            //Задание 3
+            //int a = 1;
+            //while(a<=5)
+            //{
+            //    Console.WriteLine("ПРИВЕТ!");
+            //    a++;
+            //}
+            //Console.ReadKey();
+
+            //Задание 4
+            //а
+            //for (int a = 1; a <= 18; a++)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine('_');
+                
+            //}
+            //Console.ReadKey();
+            //б
+            //for(int a=1;a<=25;a++)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine('|');
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
